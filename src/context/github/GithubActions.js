d6b6998be4495c7710d1c589b49034cf36096394
Shruti Jaiswal <jaiswalshruti1102@gmail.com>
2022-01-21 21:34:19 +0530

    token removed

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN =process.env.REACT_APP_GITHUB_TOKEN
+
 
 const github = axios.create({
-  baseURL: GITHUB_URL,
-  headers: {  Authorization: `token ${GITHUB_TOKEN}`}
+  baseURL: GITHUB_URL
+ 
 })
 
 export const searchUsers = async (text) => {
